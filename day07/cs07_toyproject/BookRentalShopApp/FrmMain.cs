a1dd00f6a91c5f627a4c5df8308eaf94e91ea05f
breadcoffee <printf0543@gmail.com>
2024-04-19 17:14:15 +0900

    7일차 도서관리시스템

---
+++ day07/cs07_toyproject/BookRentalShopApp/FrmMain.cs
@@ -0,0 +1,16 @@
+using Ax.Fw.MetroFramework.Forms;
+namespace BookRentalShopApp
+{
+    public partial class FrmMain : BorderlessForm // MetroForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ day07/cs07_toyproject/NewBookRentalShopApp/FrmMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace NewBookRentalShopApp
+{
+    public partial class FrmMain : MetroForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        // 폼로드 이벤트핸들러. 로그인창을 먼저 띄워야함
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            FrmLogin frm = new FrmLogin();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            frm.TopMost = true; // 실행 시 커서가 윈폼에 올라가게
+            frm.ShowDialog();
+        }
+    }
+}
