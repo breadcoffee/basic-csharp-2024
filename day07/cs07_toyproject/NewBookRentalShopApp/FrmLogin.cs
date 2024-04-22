b26fc7a47cefb3324040c45e6256aca274d9ecb6
breadcoffee <printf0543@gmail.com>
2024-04-22 17:19:24 +0900

    8일차 도서관리 프로젝트 진행중

--- day07/cs07_toyproject/NewBookRentalShopApp/FrmLogin.cs
+++ day07/cs07_toyproject/NewBookRentalShopApp/FrmLogin.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -72,6 +73,8 @@ namespace NewBookRentalShopApp
         // 로그인 DB 처리
         private bool LoginProcess()
         {
+            var md5Hash = MD5.Create();
+
             string userId = TxtUserId.Text; // 현재 DB로 넘기는 값
             string password = TxtPassword.Text;
             string chkUserId = string.Empty; // DB에서 넘어온 값
@@ -101,7 +104,7 @@ namespace NewBookRentalShopApp
                 SqlCommand cmd = new SqlCommand(query, conn);
                 // @userId, @password 파라미터 할당
                 SqlParameter prmUserId = new SqlParameter("@userId", userId);
-                SqlParameter prmPassword = new SqlParameter("@password", password);
+                SqlParameter prmPassword = new SqlParameter("@password", GetMd5Hash(md5Hash, password));
                 cmd.Parameters.Add(prmUserId);
                 cmd.Parameters.Add(prmPassword);
 
@@ -137,6 +140,20 @@ namespace NewBookRentalShopApp
             {
                 TxtPassword.Focus(); // 패스워드로 포커스 이동
             }
+        }
+
+        // MD5 해시 알고리즘 암호화
+        // 1234 --> 01011000 -> 110010100100110 -> x65xAEx11 처럼 변환
+        string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            // 입력문자열을 byte배열로 변환한 뒤 MD5 해시처리
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder builder = new StringBuilder(); // 문자열을 좀 더 쉽게 쓰게 만들어주는 클래스
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2")); // 16진수 문자로 각 글자를 전부 변환
+            }
+            return builder.ToString();
         }
     }
 }
--- day07/cs07_toyproject/NewBookRentalShopApp/FrmMain.cs
+++ day07/cs07_toyproject/NewBookRentalShopApp/FrmMain.cs
@@ -13,6 +13,9 @@ namespace NewBookRentalShopApp
 {
     public partial class FrmMain : MetroForm
     {
+        // 각 화면을 초기화
+        FrmLoginUser frmLoginUser = null; // 객체를 메서드로 생성
+
         public FrmMain()
         {
             InitializeComponent();
@@ -25,6 +28,39 @@ namespace NewBookRentalShopApp
             frm.StartPosition = FormStartPosition.CenterScreen;
             frm.TopMost = true; // 실행 시 커서가 윈폼에 올라가게
             frm.ShowDialog();
+        }
+
+        private void MnuLoginUsers_Click(object sender, EventArgs e)
+        {
+            // 이미 창이 열려있으면 새로 생성할 필요가 없다.
+            // 이런 작업을 안하면 메뉴 클릭마다 새로운 창이 열림
+            frmLoginUser = ShowActiveForm(frmLoginUser, typeof(FrmLoginUser)) as FrmLoginUser;
+        }
+
+        Form ShowActiveForm(Form form, Type type)
+        {
+            if (form == null) // 창을 처음 열었을때
+            {
+                form = Activator.CreateInstance(type) as Form; // 타입은 클래스 타입
+                form.MdiParent = this; // 자식창의 부모는 FrmMain
+                form.WindowState = FormWindowState.Normal;
+                form.Show();
+            }
+            else
+            {
+                if (form.IsDisposed) // 창이 한번 닫혔으면
+                {
+                    form = Activator.CreateInstance(type) as Form; // 타입은 클래스 타입
+                    form.MdiParent = this; // 자식창의 부모는 FrmMain
+                    form.WindowState = FormWindowState.Normal;
+                    form.Show();
+                }
+                else // 창을 그냥 최소화
+                {
+                    form.Activate(); // 화면에 열려있는 창을 활성화
+                }
+            }
+            return form;
         }
     }
 }
